4e11383d23e7b8267575070946aec1a12d558a27
gerosantacruz <geronimo.santacruz2017@gmail.com>
2019-10-15 22:36:05 -0300

    path created also folders

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import exphbs  from 'express-handlebars';
+import path    from 'path';
 
 //Initialization
 const app = express();
 
 //setting
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views')); 
+app.set('.hbs', exphbs({
+    extname:'.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
+}))
+
 
 //Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 
 //Routes
 
